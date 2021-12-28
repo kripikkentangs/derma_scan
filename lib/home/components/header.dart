3b12d33e8f7871075b27d2eb454b9b8d75887842
bayustys <94543338+kripikkentangs@users.noreply.github.com>
2021-12-28 15:23:53 +0700

    fix header.dart

--- lib/home/components/header.dart
+++ lib/home/components/header.dart
@@ -8,52 +8,55 @@ class Header extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: 20, right: 20),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => HeaderContent()),
-          );
-        },
-        child: Stack(
-          children: [
-            Container(
-              height: 200,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(5)),
-                image: DecorationImage(
-                    image: AssetImage('assets/images/kanker-kulit.jpg'),
-                    fit: BoxFit.cover),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3),
-                  ),
-                ],
+      child: Stack(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              image: DecorationImage(
+                  image: AssetImage('assets/images/kanker-kulit.jpg'),
+                  fit: BoxFit.cover),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HeaderContent()),
+                  );
+                },
               ),
             ),
-            Positioned(
-              bottom: 20,
-              left: 15,
-              child: Text(
-                'Apa Itu Kanker Kulit?',
-                style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 20,
-                    color: Colors.white,
-                    shadows: [
-                      Shadow(
-                        color: Color(0xFF4D4D4D),
-                        offset: Offset(1, 1.7),
-                      ),
-                    ]),
-              ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 15,
+            child: Text(
+              'Apa Itu Kanker Kulit?',
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 20,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      color: Color(0xFF4D4D4D),
+                      offset: Offset(1, 1.7),
+                    ),
+                  ]),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
